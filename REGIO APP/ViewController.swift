7040c31c38ea31316bbfa40a727f80b8de19182a
Sebastian Steiner <sebastian.steiner@sz-ybbs.ac.at>
2021-12-21 19:23:45 +0100

    Initial Commit

---
+++ REGIO APP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  REGIO APP
+//
+//  Created by Sebastian Steiner on 21.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
